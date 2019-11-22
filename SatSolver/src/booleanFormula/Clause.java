3c659b59104b2880df0afd44eb4b425868cca36b
Serge Durand <serge.durand@ymail.com>
2019-11-22 18:25:21 +0100

    Create first classes

---
+++ SatSolver/src/booleanFormula/Clause.java
@@ -0,0 +1,5 @@
+package booleanFormula;
+
+public class Clause {
+
+}
---
+++ SatSolver/src/sudoku/Sudoku.java
@@ -0,0 +1,16 @@
+package sudoku;
+
+/**
+ * @author Serge
+ *
+ *This class handles Sudoku: display, reading from a file, converting to a SAT-problem
+ */
+public class Sudoku {
+	
+	private int[][] grid = new int[9][9];
+	
+	public Sudoku(int[] list_int) {
+		
+	}
+		
+}
