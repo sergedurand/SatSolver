b77e7d9edf8b40e0c266d40a2bbb4dd051bd1a4f
serge <serge.durand@ymail.com>
2020-05-11 23:07:24 +0200

    start solver twl

--- SatSolver/src/solver/Twl.java
+++ SatSolver/src/solver/Twl.java
@@ -1,5 +1,7 @@
 package solver;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import booleanFormula.CNF;
@@ -7,13 +9,212 @@ import booleanFormula.CNFException;
 import booleanFormula.Clause;
 
 public class Twl implements Solver {
+	boolean solved = false;
+	int[] interpretation;
 
 	@Override
-	public boolean solve(CNF formula, int timeout) throws CNFException, SolverTimeoutException {
+	public boolean solve(CNF phi,int timeout) throws CNFException, SolverTimeoutException {
+		long start_time = System.nanoTime();
+		
+		//we first get rid of preexisting unit clauses
+		keepPropagating(phi); 
+		if(phi.hasEmptyClause()) {
+			System.out.println("trivially false");
+			this.solved = false;
 			return false;
+		}
+		
+		phi.initWatchedLiterals();//since we got rid of unit clauses we're sure all active clauses are watched by 2 literals
+		LinkedList<Integer> VariablesLeft = phi.getUnassigned();
+
+		// stack to be used when backtracking
+		LinkedList<Integer> VariablesExplored = new LinkedList<Integer>();
+		int var = -1;
+		boolean backtracking = false;
+		long elapsed_time;
+		LinkedList<Integer> TempExplored = new LinkedList<Integer>();
+		LinkedList<Integer> varToRemove = new LinkedList<Integer>();
+		while(true) {
+			elapsed_time = (System.nanoTime()-start_time)/1_000_000_000;
+			if(elapsed_time > timeout) {
+				throw new SolverTimeoutException("Over "+ timeout + " seconds");
+			}
+			int val;
+			int[] assigned_var = {0};
+			boolean empty = false;
+			VariablesLeft = phi.getUnassigned();
+			if(!backtracking) {
+				//case of a fresh variable: start from val = 0
+				try {
+					var = VariablesLeft.pop();
+				}catch(NoSuchElementException e) {
+					//we have no more variables left and haven't found a conflict, it's SAT
+					//in the weird case the formula is empty with no variable:
+					if(phi.hasEmptyClause()) {
+						this.solved = false;
+						return false;
+					}
+					this.solved = true;
+					this.interpretation = phi.getInterpretation();
+					return true;
+				}
+				//we have a variable to check.
+				val = 0;
+
+				phi.getVariables().setVal(var, val);
+				empty = false;
+				TempExplored = new LinkedList<Integer>();
+				
+				while(true) {
+					deactivateSatClauses(var, phi);
+					empty = phi.hasEmptyClause();
+					if(empty) {
+						break;
+					}
+					assigned_var = unitPropagation(phi);
+					if(assigned_var[0] == -1) {
+						break;
+					}
+					TempExplored.add(assigned_var[0]);		
+					phi.getVariables().setVal(assigned_var[0], assigned_var[1]);
+					deactivateSatClauses(assigned_var[0], phi);
+					empty = phi.hasEmptyClause();
+					if(empty) {
+						break;
+					}
+				}
+
+				
+				if(!empty) {
+					//we found no empty clause. We can keep looking with the next variable.
+					//add a checkpoint for future backtracking first
+					VariablesExplored.add(-1);
+					VariablesExplored.add(var);
+					VariablesExplored.addAll(TempExplored);
+					backtracking = false;
+					continue;
+				}else {
+					//conflict found, we try the other assignment
+					
+//					LinkedList<Integer> varToRemove = new LinkedList<Integer>();
+//					int var_temp;
+//					while((var_temp = VariablesExplored.removeLast())!= -1) {
+//						varToRemove.add(var_temp);
+//					}
+//					var = varToRemove.removeLast();
+//					for(int i : varToRemove) {
+//						phi.reactivateSetClause(i);
+//					}
+					
+
+					val = 1;
+					
+					if(TempExplored.size()>0) {
+						for(int i : TempExplored) {
+							phi.reactivateSetClause(i);
+						}
+					}
+					
+					phi.reactivateSetClause(var);
+					phi.getVariables().setVal(var, val);
+					assigned_var[0] = 0;
+					TempExplored = new LinkedList<Integer>();
+					while(true) {
+						deactivateSatClauses(var, phi);
+						empty = phi.hasEmptyClause();
+						if(empty) {
+							break;
+						}
+						assigned_var = unitPropagation(phi);
+						if(assigned_var[0] == -1) {
+							break;
+						}
+						TempExplored.add(assigned_var[0]);
+						phi.getVariables().setVal(assigned_var[0], assigned_var[1]);
+						deactivateSatClauses(assigned_var[0], phi);
+						empty = phi.hasEmptyClause();
+						if(empty) {
+							break;
+						}
+					}
+					if(!empty) {
+						//we can continue, no backtracking
+						VariablesExplored.add(var);
+						VariablesExplored.addAll(TempExplored);
+						backtracking = false;
+						continue;
+					}else {
+						//we have an empty clause on second assignement: need to backtrack
+						phi.reactivateSetClause(var);
+						backtracking = true;
+						continue;
+					}
+				}
+				
+			}else { //we are backtracking
+				varToRemove = new LinkedList<Integer>();
+				if(TempExplored.size()>0) {
+					for(int i : TempExplored) {
+						phi.reactivateSetClause(i);
+					}
+				}
+				int var_temp;
+				try {
+					while((var_temp = VariablesExplored.removeLast())!= -1) {
+						varToRemove.add(var_temp);
+					}
+				}catch(NoSuchElementException e) {
+					//no checkpoint found: can't backtrack further: UNSAT
+					this.solved = false;
+					return false;
+				}
+
+				var = varToRemove.removeLast();
+				for(int i : varToRemove) {
+					phi.reactivateSetClause(i);
+				}
+				phi.reactivateSetClause(var);
+				val = 1;
+				phi.getVariables().setVal(var, val);
+				//no need to get a specific val, when backtracking we know we have only assignment 1 left.
+				//no need to add to explored variables: we won't backtrack again to here
+				assigned_var[0] = 0;
+				TempExplored = new LinkedList<Integer>();
+				while(true) {
+					deactivateSatClauses(var, phi);
+					empty = phi.hasEmptyClause();
+					if(empty) {
+						break;
+					}
+					assigned_var = unitPropagation(phi);
+					if(assigned_var[0] == -1) {
+						break;
+					}
+					TempExplored.add(assigned_var[0]);
+					phi.getVariables().setVal(assigned_var[0], assigned_var[1]);
+					deactivateSatClauses(assigned_var[0], phi);
+					empty = phi.hasEmptyClause();
+					if(empty) {
+						break;
+					}
+				}
+				if(!empty) {
+					//we can continue
+					VariablesExplored.add(var);
+					VariablesExplored.addAll(TempExplored);
+					backtracking = false;
+					continue;
+				}else {
+					//we have an empty clause on second assignement we have to try to backtrack further
+					phi.reactivateSetClause(var);
+					backtracking = true;
+					continue;
+				}
+			}
+		}
 	}
 	
-	public void deactivateSatClauses(int var_id,CNF phi) throws CNFException {
+	public void deactivateSatClausesTwl(int var_id,CNF phi) throws CNFException {
 		Set<Integer> clauses_id = phi.getActiveClauses();
 		int lit_pos;
 		int lit_neg;
@@ -64,6 +265,46 @@ public class Twl implements Solver {
 			}
 		}
 		return res;	
+	}
+	
+	public LinkedList<Integer> keepPropagating(CNF phi) throws CNFException{
+		LinkedList<Integer> var_learned = new LinkedList<Integer>();
+		boolean empty = false;
+		int[] assigned_var = {-1};
+		while(true) {
+			assigned_var = unitPropagation(phi);
+			if(assigned_var[0] == -1) {
+				break;
+			}
+			var_learned.add(assigned_var[0]);
+			phi.getVariables().setVal(assigned_var[0], assigned_var[1]);
+			deactivateSatClauses(assigned_var[0], phi);
+			empty = phi.hasEmptyClause();
+			if(empty) {
+				break;
+			}
+		}
+		return var_learned;
+	}
+	
+	public LinkedList<Integer> keepPropagatingTwl(CNF phi,int lit_id) throws CNFException{
+		LinkedList<Integer> var_learned = new LinkedList<Integer>();
+		boolean empty = false;
+		int[] assigned_var = {-1};
+		while(true) {
+			assigned_var = unitPropagation(phi);
+			if(assigned_var[0] == -1) {
+				break;
+			}
+			var_learned.add(assigned_var[0]);
+			phi.getVariables().setVal(assigned_var[0], assigned_var[1]);
+			deactivateSatClauses(assigned_var[0], phi);
+			empty = phi.hasEmptyClause();
+			if(empty) {
+				break;
+			}
+		}
+		return var_learned;
 	}
 
 	@Override
